63c9c0fd2a1495b1cfe1dfe961c9d83a55219587
hamata <ahmed.adham.1543@gmail.com>
2023-12-15 00:51:10 +0200

    added iterator pattern

---
+++ Model/StackIterator.java
@@ -0,0 +1,37 @@
+package Model;
+
+import eg.edu.alexu.csd.oop.game.GameObject;
+import java.util.Stack;
+
+/**
+ * @author Ahmed Mahmoud
+ */
+public class StackIterator implements Iterator<GameObject>{
+
+    private final Stack<GameObject> currentStack;
+    
+    public StackIterator(Stack<GameObject> givenStack){
+        this.currentStack = cloneStack(givenStack);
+    }
+    
+    private static Stack<GameObject> cloneStack(Stack<GameObject> givenStack){
+        Stack<GameObject> clone = new Stack<>();
+        for(var item : givenStack){
+            clone.push(item);
+        }
+        return clone;
+    }
+    
+    @Override
+    public boolean hasNext() {
+        currentStack.pop();
+        return !currentStack.isEmpty();
+    }
+
+    @Override
+    public GameObject next() {
+        currentStack.pop();
+        return currentStack.peek();
+    }
+
+}
